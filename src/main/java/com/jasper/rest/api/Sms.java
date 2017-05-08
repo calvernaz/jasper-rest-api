d6a04f6c11f9ab2a464d8ee567afe2054f516fca
Cesar Alvernaz <cesar.alvernaz@gmail.com>
2017-05-08 22:01:36 +0100

    Remove unnecessary parameter

--- src/main/java/com/jasper/rest/api/Sms.java
+++ src/main/java/com/jasper/rest/api/Sms.java
@@ -6,25 +6,19 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 @JsonSerialize
 public class Sms {
-    private final String iccid;
     private final String messageText;
     private final String messageEncoding;
     private final Integer dataCoding;
     private final Integer tpvp;
 
     @JsonCreator
-    public Sms(@JsonProperty("iccid") String iccid, @JsonProperty("messageText") String messageText,
+    public Sms(@JsonProperty("messageText") String messageText,
                @JsonProperty("messageEncoding") String messageEncoding, @JsonProperty("dataCoding") Integer dataCoding,
                @JsonProperty("tpvp") Integer tpvp) {
-        this.iccid = iccid;
         this.messageText = messageText;
         this.messageEncoding = messageEncoding;
         this.dataCoding = dataCoding;
         this.tpvp = tpvp;
-    }
-
-    public String getIccid() {
-        return iccid;
     }
 
     public String getMessageText() {
